d83beef029c29b192529c369c204b7b790f3abf1
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-03-29 13:28:45 +0530

    Time: 19 ms (54.81%), Space: 24.7 MB (51.96%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -12,24 +12,15 @@ public:
         while(l<=h){
           int mid=l+(h-l)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
-            if(nums[mid]==nums[mid-1]){
-              if((mid-1)%2==1){
-                h=mid-1;
-              }
-                else{
-                  l=mid+1;
-                }
+            if(nums[mid]==nums[mid-1] && mid%2==1 || mid%2==0 && nums[mid]==nums[mid+1]){
+              
+                l=mid+1;
             }
-            else if(nums[mid]==nums[mid+1]){
-               if(mid%2==0){
-                   l=mid+1;
-                }
-                else{
-                         h=mid-1;
-                }
-            }
+            else {
+               h=mid-1;
            
         }
+ }
         return -1;
     }
 };
