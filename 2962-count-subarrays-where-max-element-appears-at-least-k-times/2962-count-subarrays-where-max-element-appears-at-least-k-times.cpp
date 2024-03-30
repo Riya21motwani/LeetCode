63cf14346ce01d214af67cb463b168a844a9b317
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-03-30 09:19:01 +0530

    Time: 117 ms (90.43%), Space: 120 MB (61.72%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,25 +1,32 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
+    
     long long countSubarrays(vector<int>& nums, int k) {
-        int mx=INT_MIN;
-        for(auto it : nums){
-            mx=max(mx,it);
-        }
-        int n=nums.size();
-        int freq=0;
-        int start=0;
-        long long ans=0;
-        for(int end=0;end<n;end++){
-            // Growing Phase
-            if(nums[end]==mx)
-                freq++;
-            while(freq==k){//Shrinking Phase
-                if(nums[start]==mx)
-                    freq--;
-                start++;
+        long long n=nums.size();
+        
+         long long res=0;
+       long long cnt_maxi=0;
+       int maxi=*max_element(nums.begin(),nums.end());
+        long long i=0,j=0;
+        while(j<n){
+         
+             if(nums[j]==maxi){
+                cnt_maxi++;
+                 
+             }
+               while(cnt_maxi>=k){
+                    res+=n-j;
+                     if(nums[i]==maxi){
+                      cnt_maxi--;
+                        
+                     }
+                   i++;
+                
+               
             }
-            ans+=start;
+            j++;
         }
-        return ans;
+        return res;
     }
 };
