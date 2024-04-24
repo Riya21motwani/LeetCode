05e3c29c5f7d707b411b0a8a3258030b058dc3fd
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-04-24 13:23:42 +0530

    Time: 32 ms (57.01%), Space: 21.4 MB (30.32%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     
+    int maximum(vector<int>& piles){
+          int n=piles.size();
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+       maxi=max(maxi,piles[i]);
+
+        }
+        return maxi;
+    }
+    
    int isPossible(vector<int>& piles, int h,int k){
     int t=0;
     for(int i=0;i<piles.size();i++){
@@ -10,10 +20,10 @@ public:
     return t;
 }
     int minEatingSpeed(vector<int>& piles, int h) {
-        sort(piles.begin(),piles.end());
+     
         int ans=0;
         int s=1;
-        int e=piles[piles.size()-1];
+        int e=maximum(piles);
         while(e>=s){
             int mid=e-(e-s)/2;
             if(isPossible(piles,h,mid)<=h){
