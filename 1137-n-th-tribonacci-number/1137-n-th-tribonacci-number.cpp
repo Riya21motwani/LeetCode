102e0c3bead42e78bbba29a8c13dcb7bf16f6f5f
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-05-04 09:42:04 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (50.11%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int dp[38];
-    int find( int n){
-       if(n==0)return 0;
-        if(n==1)return 1;
-        if(n==2)return 1;
-        if(dp[n]!=-1){
-            return dp[n];
+    
+    int tribonacci( int n){
+       int dp[38];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=37;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
-        return dp[n]= find(n-1)+find(n-2)+find(n-3);
-    
-    }
-    
-    
-    
-    int tribonacci(int n) {
-       memset(dp,-1,sizeof(dp));
-        return find(n);
+        return dp[n];
     }
 };
