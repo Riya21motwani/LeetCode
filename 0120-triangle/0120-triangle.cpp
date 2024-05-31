a24cf40a39a2c2886fb89a819f43de65dfa6c889
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-05-31 06:43:05 +0530

    Time: 8 ms (26.98%), Space: 11.3 MB (66.77%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     
-    int f(int i,int j, vector<vector<int>>& triangle, vector<vector<int>>&dp){
-        int n=triangle.size();
-        if(i==n-1){
-            return triangle[i][j];
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        int d=triangle[i][j]+f(i+1,j,triangle,dp);
-        int dia=triangle[i][j]+f(i+1,j+1,triangle,dp);
-        return dp[i][j]=min(d,dia);
-    }
+    
     
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(0,0,triangle,dp);
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                 if(i==n-1){
-                     dp[i][j]= triangle[i][j];
-                  }
-                   int d=triangle[i][j]+f(i+1,j,triangle,dp);
-                 int dia=triangle[i][j]+f(i+1,j+1,triangle,dp);
-                dp[i][j]=min(d,dia);
+    for(int j=0;j<n;j++)dp[n-1][j]=triangle[n-1][j];
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                 
+                   int d=triangle[i][j]+dp[i+1][j];
+                   int dia=triangle[i][j]+dp[i+1][j+1];
+                  dp[i][j]=min(d,dia);
             }
         }
-        return dp[n-1][n-1];
+        return dp[0][0];
     }
 };
