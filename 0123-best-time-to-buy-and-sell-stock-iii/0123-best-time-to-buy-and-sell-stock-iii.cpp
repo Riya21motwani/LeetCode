3a1835f69a919b7cb85d497914c0b84193425914
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-06-12 12:54:18 +0530

    Time: 443 ms (36.08%), Space: 209.6 MB (43.97%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -7,19 +7,45 @@ public:
         if(ind==n || limit==0)return 0;
         if(dp[ind][buy][limit]!=-1)return dp[ind][buy][limit];
         if(buy==1){
-            return  dp[ind][buy][limit]=max(-prices[ind]+f(ind+1,0,limit,prices,dp) , f(ind+1,1,limit,prices,dp));
+            return  dp[ind][buy][limit]=max(-prices[ind]+f(ind+1,0,limit,prices,dp) ,                          f(ind+1,1,limit,prices,dp));
         }
         
             return dp[ind][buy][limit]=max(prices[ind]+f(ind+1,1,limit-1,prices,dp),f(ind+1,0,limit,prices,dp));
         
-    
-          
     }
     
     
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-         vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(0,1,2 ,prices,dp);
+         int n=prices.size();
+          vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+          // return f(0,1,2 ,prices,dp);
+        
+        for(int buy=0;buy<=1;buy++){
+            for(int limit=0;limit<=2;limit++){
+                dp[n][buy][limit]=0;
+            }
+        }
+        
+        for(int ind=0;ind<=n;ind++){
+            for(int buy=0;buy<=1;buy++){
+                dp[ind][buy][0]=0;
+            }
+        }
+        
+        
+          
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int limit=1;limit<=2;limit++){
+                      if(buy==1){
+                       dp[ind][buy][limit]=max(-prices[ind]+dp[ind+1][0][limit], dp[ind+1][1][limit]);
+                     }
+                  else{
+                    dp[ind][buy][limit]=max(prices[ind]+dp[ind+1][1][limit-1],dp[ind+1][0][limit]);
+                }
+             }
+            }
+        }
+        return dp[0][1][2];
     }
 };
