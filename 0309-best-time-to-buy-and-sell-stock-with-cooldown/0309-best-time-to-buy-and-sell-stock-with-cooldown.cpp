e2cda5290998b6724b93dedf2e397d37a65b646d
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-06-13 11:12:04 +0530

    Time: 4 ms (53.98%), Space: 14.1 MB (51.67%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -27,14 +27,13 @@ public:
         }
         
         for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                 if(buy==1){
-                    profit=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
-                 }else{
-                   profit=max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
-                 }
-                 dp[ind][buy]=profit;
-            }
+           
+                    dp[ind][1]=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+                
+                   dp[ind][0]=max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
+                 
+               
+            
         }
         
         return dp[0][1];
