2ca207c455f003a4d8137ddc95744e82330f67b9
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-06-14 10:50:14 +0530

    Time: 16 ms (49.41%), Space: 11.5 MB (15.82%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-     int n=nums.size();    
-    vector<int> dp(n,1);
-    vector<int> hash(n,1);
-    sort(nums.begin() ,nums.end()) ;
-    for(int i=0; i<=n-1; i++){
+            int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>dp(n,1);
+        vector<int>hash(n,1);//index telling 
         
-        hash[i] = i; 
-        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
-            
-            if(nums[i]%nums[prev_index]==0 && 1 + dp[prev_index] > dp[i]){
-                dp[i] = 1 + dp[prev_index];
-                hash[i] = prev_index;
+    
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int previ=0;previ<i;previ++){
+                if(nums[i]%nums[previ]==0 && 1+dp[previ]>dp[i]){
+                    dp[i]=1+dp[previ];
+                    hash[i]=previ;
+                }
             }
         }
-    }
-    
-    int ans = -1;
-    int lastIndex =-1;
-    
-    for(int i=0; i<=n-1; i++){
-        if(dp[i]> ans){
-            ans = dp[i];
-            lastIndex = i;
+        int maxi=-1;
+        int lastindex=-1;
+        for(int i=0;i<n;i++){
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                lastindex=i;
+            }
         }
-    }
-    
-    vector<int> temp;
-    temp.push_back(nums[lastIndex]);
-    
-    while(hash[lastIndex] != lastIndex){ 
-        lastIndex = hash[lastIndex];
-        temp.push_back(nums[lastIndex]);    
-    }
-    
-   
-    reverse(temp.begin(),temp.end());
+        
+        
+        
+        vector<int>temp;
+        
+        temp.push_back(nums[lastindex]);
+        while(hash[lastindex]!=lastindex){
+            lastindex=hash[lastindex];
+            temp.push_back(nums[lastindex]);
+        }
+        reverse(temp.begin(),temp.end());
         return temp;
     }
 };
