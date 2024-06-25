f8b425d1bb606423a56c0b864f8ce3e6e11d1172
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-06-25 17:34:37 +0530

    Time: 92 ms (52.10%), Space: 108.9 MB (84.37%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        int n = nums.size();
+       int n = nums.size();
 
-        int flips = 0;
-        vector<bool> is(n, false);
-        int flipC = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(i >= k && is[i-k] == true){
-                flipC--;                     
+        int res=0;
+        int flipcnt=0;
+        vector<bool>isflipped(n,0);
+        for(int i=0;i<n;i++){
+            if(i>=k && isflipped[i-k]==1){
+                flipcnt--;
             }
             
-            if(flipC % 2 == nums[i]) {
-                if(i+k > n){
-                    return -1;
-                }
-                flipC++;
-                is[i] = true;
-                flips++;
+            if(flipcnt%2==nums[i]){
+                if(i+k>n)return -1;
+                res++;
+                flipcnt++;
+                isflipped[i]=1;
             }
         }
-
-        return flips;
+        return res;
+        
     }
 };
