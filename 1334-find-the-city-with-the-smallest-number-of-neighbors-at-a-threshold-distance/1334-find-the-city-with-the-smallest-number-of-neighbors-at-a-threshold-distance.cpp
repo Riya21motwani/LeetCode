ff819ce2614f0bb0e3fc06ef8a0db5cd6d4f4b78
Riya21motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-06-29 07:24:19 +0530

    Time: 736 ms (5.05%), Space: 215.5 MB (5.01%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
+    typedef pair<int,int>p;
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         
-       	vector<vector<int>> dis(n, vector<int> (n, INT_MAX));
-        for(auto it:edges){
-            dis[it[0]][it[1]]=it[2];
-            dis[it[1]][it[0]]=it[2];
+       	vector<vector<int>>adj[n];
+        for(auto it: edges){
+           adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
+        int citycnt=1e9;
+        int cityno=-1;
+       for(int i=0;i<n;i++) {
         
-         for(int i=0;i<n;i++)dis[i][i]=0;
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    	if (dis[i][k] == INT_MAX || dis[k][j] == INT_MAX)
-						continue;
-                    dis[i][j]=min(dis[i][j] , dis[i][k]+dis[k][j]);
+        vector<int>dis(n,1e9);
+        priority_queue<p,vector<p>,greater<p>>pq;
+        pq.push({0,i});
+           dis[i]=0;
+        while(!pq.empty()){
+             int node=pq.top().second;
+             int distance=pq.top().first;
+            pq.pop();
+            for(auto it:adj[node]){
+                int newnode=it[0];
+                int newdistance=it[1];
+                if(distance+newdistance<dis[newnode]){
+                    dis[newnode]=newdistance+distance;
+                    pq.push({newdistance+distance,newnode});
                 }
             }
+            
         }
-       int citycnt=1e9;
-        int cityno=0;
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(dis[i][j]<=distanceThreshold ){
-                    cnt++;
-                }
-            }
-            if(cnt<=citycnt){
-                citycnt=cnt;
-                cityno=i;
-            }
+           
+           int cnt=0;
+           for(int j=0;j<n;j++){
+               if(dis[j]<= distanceThreshold){
+                   cnt++;
+               }
+           }
+        if(cnt<=citycnt){
+            citycnt=cnt;
+            cityno=i;
         }
+       }
         return cityno;
     }
 };
