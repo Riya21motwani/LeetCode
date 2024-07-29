4808bc3abcb99993dea3d1209e3fb4a89189f0b6
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-07-29 19:38:05 +0530

    Time: 27 ms (80.51%), Space: 11.5 MB (63.96%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     
-    
     int numTeams(vector<int>& rating) {
         int n=rating.size();
-        int cnt=0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
-                    if(rating[i]<rating[j] && rating[j]<rating[k]  ||  rating[i]>rating[j] && rating[j]>rating[k]){
-                        cnt++;
-                    }
+        int total=0;
+        for(int j=1;j<n-1;j++){
+            int cntsmallerleft=0;
+              int    cntlargerleft=0;
+              int    cntsmallerright=0;
+                 int cntlargerright=0;
+            for(int i=0;i<j;i++){
+                if(rating[i]<rating[j]){
+                    cntsmallerleft++;
                 }
+                if(rating[i]>rating[j]){
+                    cntlargerleft++;
+                }
+                
             }
+            for(int k=j+1;k<n;k++){
+                if(rating[k]<rating[j]){
+                    cntsmallerright++;
+                }
+                if(rating[k]>rating[j]){
+                    cntlargerright++;
+                }
+                
+            }
+            
+            total+= cntsmallerleft*cntlargerright +cntlargerleft*cntsmallerright;
+            
         }
-        return cnt;
+        
+        return total;
           
     }
 };
