b7e24ecadb3fb7926de262efc5c515a05a0aa7b7
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-08-13 06:43:46 +0530

    Time: 32 ms (17.34%), Space: 24.8 MB (17.50%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,41 +1,29 @@
 class KthLargest {
 public:
     vector<int> stream;
-    int k;
-
+    int K;
+    priority_queue<int,vector<int>,greater<int>>pq;
     KthLargest(int k, vector<int>& nums) {
-        this->k = k;
-        for (int num : nums) {
-            stream.push_back(num);
+        K=k;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+            if(pq.size()>K){
+                pq.pop();
+            }
         }
-        sort(stream.begin(), stream.end());
     }
 
     int add(int val) {
-        int index = getIndex(val);
+       pq.push(val);
+        if(pq.size()>K){
+            pq.pop();
+        }
         
-        stream.insert(stream.begin() + index, val);
-        return stream[stream.size() - k];
+        return pq.top();
     }
 
-private:
-    int getIndex(int val) {
-        int left = 0;
-        int right = stream.size() - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int midValue = stream[mid];
-            if (midValue == val) return mid;
-            if (midValue > val) {
-                
-                right = mid - 1;
-            } else {
-               
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
+
 };
 /**
  * Your KthLargest object will be instantiated and called as such:
