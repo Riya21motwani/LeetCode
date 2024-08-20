f1d88d5c94b22fc1a9a7267e570327f0805529ab
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-08-20 08:04:57 +0530

    Time: 95 ms (5.40%), Space: 388.7 MB (8.65%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,27 +1,33 @@
 class Solution {
-    int n;
+    
 
 public:
+    
+    int f(int curr, int prevc, int n ,  vector<vector<int>>&dp){
+     
+        if(curr==n){
+            return 0;
+        }
+        if(curr>n){
+            return 100000000;
+        }
+        if(dp[curr][prevc]!=-1){
+            return dp[curr][prevc];
+        }
+        int cap=2+f(curr+curr,curr,n , dp);
+        int p=1+f(curr+prevc,prevc,n , dp);
+        return dp[curr][prevc]=min(cap,p);
+    }
+    
+    
+    
     int minSteps(int n) {
-        if (n == 1) return 0;
-        this->n = n;
-       
-        return 1 + minStepsHelper(1, 1);
+         
+        if(n==1){
+            return 0;
+        }
+        vector<vector<int>>dp(1001,vector<int>(1001,-1));
+        return 1+f(1,1,n , dp);
     }
-
-private:
-    int minStepsHelper(int currLen, int pasteLen) {
-        
-        if (currLen == n) return 0;
-       
-        
-        if (currLen > n) return 1000;
-
-      
-        int opt1 = 2 + minStepsHelper(currLen * 2, currLen);
-      
-        int opt2 = 1 + minStepsHelper(currLen + pasteLen, pasteLen);
-
-        return min(opt1, opt2);
-    }
+    
 };
