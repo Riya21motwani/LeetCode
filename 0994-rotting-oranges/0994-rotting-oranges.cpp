bc1e62c32d16dc030f6f93e051446a1d51629711
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-09-10 08:11:39 +0530

    Time: 7 ms (42.87%), Space: 16.6 MB (23.94%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,44 +3,48 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
           int n=grid.size();
           int m=grid[0].size();
-      
-           int cnt1=0;
-        int vis[n][m];
-           queue<pair<pair<int,int>,int>>q;
-          for(int i=0;i<n;i++){
-              for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                }else {
-                    vis[i][j]=0;
-                }
-                  if(grid[i][j]==1)cnt1++;
-              }
-          }
-          
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-        int tm=0;
+            
+          queue<pair<int,pair<int,int>>>q;
+          int cnt1=0;
         int cnt=0;
-        while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int t=q.front().second;
-            tm=max(tm,t);
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<n&&ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                    vis[nrow][ncol]=2;
-                    q.push({{nrow,ncol},t+1});
-                    cnt++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({0,{i,j}});
+                }
+                if(grid[i][j]==1){
+                    cnt1++;
                 }
             }
         }
-        if(cnt1!=cnt)return -1;
-        return tm;
         
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int maxi=0;
+        while(!q.empty()){
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+            int t=q.front().first;
+            maxi=max(maxi,t);
+            
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=drow[i]+row;
+                int ncol=dcol[i]+col;
+                
+                if(nrow>=0 &&ncol>=0 &&nrow<n &&ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    cnt++;
+                    q.push({t+1,{nrow,ncol}});
+                }
+            }
+            
+             
+        }
+        if(cnt1!=cnt){
+            return -1;
+        }
+         return maxi;
     }
 };
