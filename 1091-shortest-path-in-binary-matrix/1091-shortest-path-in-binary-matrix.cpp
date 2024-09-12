07e4b6f8b57acb1b09393e39099c9417522fbf78
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-09-12 16:49:19 +0530

    Time: 41 ms (80.46%), Space: 25.4 MB (36.68%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
+    typedef pair<int,int>p;
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        
-        if(grid[0][0]==1 || grid[n-1][n-1]==1)return -1;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1){
+            return -1;
+        }
         vector<vector<int>>dis(n,vector<int>(n,1e9));
-       queue<pair<int,pair<int,int>>>q;
-        q.push({1,{0,0}});
         dis[0][0]=1;
-        vector<pair<int,int>> dir={{0,1},{0,-1},{-1,0},{1,0},{1,1},{1,-1},{-1,1},{-1,-1}}; 
-        while(!q.empty()){
-            int distance=q.front().first;
-            int row=q.front().second.first;
-            int col=q.front().second.second;
-            q.pop();
+     queue<pair<int,pair<int,int>>>pq;
+        
+        
+        vector<pair<int,int>>dir={{-1,1},{1,1} ,{1,-1}, {-1,-1} , {0,1} , {1,0} , {-1,0} , {0,-1}};
+        pq.push({1,{0,0}});
+        while(!pq.empty()){
+            int distance=pq.front().first;
+            int row=pq.front().second.first;
+            int col=pq.front().second.second;
+            pq.pop();
+            if(row==n-1 &&col==n-1){
+                return distance;
+            }
             for(auto it:dir){
-                int nrow=row+it.first;
-                int ncol=col+it.second;
-                if(nrow>=0 &&ncol>=0 &&nrow<n&& ncol<n && grid[nrow][ncol]==0 &&  1+distance<dis[nrow][ncol]){
+              int nrow=it.first+row;
+                int ncol=it.second+col;
+                if(nrow>=0 &&ncol>=0 &&ncol<n &&nrow<n && 1+distance < dis[nrow][ncol] && grid[nrow][ncol]==0 ){
                     dis[nrow][ncol]=1+distance;
-                    q.push({1+distance,{nrow,ncol}});
+                    pq.push({1+distance, {nrow,ncol}});
                 }
             }
+            
         }
-        if(dis[n-1][n-1]==1e9)return -1;
+        
+         if(dis[n-1][n-1]==1e9)return -1;
             return dis[n-1][n-1];
     }
 };
