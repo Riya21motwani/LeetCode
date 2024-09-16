5a52c51280ad9973a425c01d4ebefc1aea561084
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-09-16 08:09:40 +0530

    Time: 21 ms (19.08%), Space: 18.7 MB (19.72%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
     
-    bool bfs(vector<int>&ans, vector<int>&indegree,  unordered_map<int,vector<int>>&adj , int numCourses){
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                   q.push(i);
-            }
-        }
-        int cnt=0;
+   bool dfs( int numCourses,  unordered_map<int,vector<int>>&adj, vector<int>&indegree , queue<int>&q  , vector<int>&ans){
+       for(int i=0;i<numCourses;i++){
+           if(indegree[i] ==0){
+                  q.push(i);
+           }
+       }
+      int cnt=0;
+      while(!q.empty()){
+           int node=q.front();
+           q.pop();
+          cnt++;
+           ans.push_back(node);
+           for(auto it:adj[node]){
+               indegree[it]--;
+              if(indegree[it]==0){
+                      q.push(it);
+              }
+           }
+       }
+       if(cnt==numCourses){
+           return true;
+       }
         
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            cnt++;
-            ans.push_back(node);
-            for(auto it:adj[node]){
-                    indegree[it]--;
-                    if(indegree[it]==0){
-                    q.push(it);
-                }
-            }
-        }
-        if(cnt==numCourses)return true;
         return false;
-    }
+       
+   }
     
     
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> ans;
         unordered_map<int,vector<int>>adj;
-        vector<int>indegree(numCourses,0);
-       
-       
-      for(auto it: prerequisites){
-            int a=it[0] ;
+vector<int>indegree(numCourses,0);
+        for(auto it:prerequisites){
+            int a=it[0];
             int b=it[1];
-          //b->a
             adj[b].push_back(a);
-          indegree[a]++;
-          
+            indegree[a]++;
         }
+        vector<int>ans;
+        queue<int>q;
+      if((dfs( numCourses, adj, indegree , q , ans )==true)){
+          return ans;
+      }
         
-        
-        if( bfs(ans,indegree,adj,numCourses)==true){
-            return ans;
-        }
         return {};
-        
     }
 };
