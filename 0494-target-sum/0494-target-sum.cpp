0546028592e6ab5ae9208aa93265cf174798b9a4
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-12-27 10:41:47 +0530

    Time: 363 ms (33.82%), Space: 11.7 MB (69.12%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    int cnt=0;
-    void f(int ind,vector<int>& nums, int target , int val){
+
+    int f(int ind,int val, vector<int>& nums){
         int n=nums.size();
         if(ind==n){
-            if(val==target){
-                cnt++;
-
+            if(val==0){
+                return 1;
             }
-            return;
+            return 0;
         }
 
-        f(ind+1,nums,target,val+nums[ind]);
-        f(ind+1,nums,target,val-nums[ind]);
+        int notake=f(ind+1,val,nums);
+
+        int take=0;
+        if(nums[ind]<=val){
+         take=f(ind+1,val-nums[ind],nums);
+        }
+
+        return take+notake;
     }
+    
     int findTargetSumWays(vector<int>& nums, int target) {
-         int n=nums.size();
-        
-         f(0,nums,target, 0);
-         return cnt;
+        int n=nums.size();
+        int totalsum=0;
+        for(int i=0;i<n;i++){
+            totalsum+=nums[i];
+        }
+
+        if(totalsum-target<0 || (totalsum-target)%2==1){
+            return 0;
+        }
+        int val=(totalsum-target)/2;
+        return f(0,val,nums);
     }
 };
