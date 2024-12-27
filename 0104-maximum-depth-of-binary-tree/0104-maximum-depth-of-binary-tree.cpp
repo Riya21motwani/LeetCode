36105c1fda726704f50d97fe52cb97798a0e4568
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2024-12-27 16:33:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,10 +11,23 @@
  */
 class Solution {
 public:
+
+    int f( TreeNode* root){
+        if(root==NULL ){
+            return INT_MIN;
+        }
+
+        if(root->left==NULL  && root->right==NULL){
+            return 1;
+        }
+        
+        int left=f(root->left);
+        int right=f(root->right);
+
+        return 1+max(left,right);
+    }
+
     int maxDepth(TreeNode* root) {
-            if(root==NULL)return 0;
-        int l=maxDepth(root->left);
-        int h=maxDepth(root->right);
-        return 1+max(l,h);
+        return f(root);
     }
 };
