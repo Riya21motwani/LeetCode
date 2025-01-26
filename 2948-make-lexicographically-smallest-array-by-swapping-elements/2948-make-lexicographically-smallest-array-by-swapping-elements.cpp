bac0be251ce619a87b21b72195a1db1ca472a5d8
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-01-26 12:32:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -2,21 +2,27 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& arr, int limit) {
-        auto sorted = arr;
-        sort(begin(sorted), end(sorted));
-        auto groups = sorted | views::chunk_by([limit](int x, int y) { return y - x <= limit; });
-        vector<vector<int>::iterator> group_iters;
-        vector<int> group_heads;
-        for (auto group : groups) {
-            group_iters.push_back(group.begin());
-            group_heads.push_back(group.front());
-        }
-        for (auto& x : arr) {
-            auto it = prev(upper_bound(group_heads.begin(), group_heads.end(), x));
-            auto& g_it = group_iters[distance(group_heads.begin(), it)];
-            x = *g_it++;
+        int n=arr.size();
+
+        for(int i=0;i<n;i++){
+            while(true){
+                
+                int ind=-1;
+                int smallval=arr[i];
+                for(int j=i+1;j<n;j++){
+                    if(abs(arr[i]-arr[j])<=limit && arr[j]<smallval){
+                        ind=j;
+                        smallval=arr[j];
+                    }
+                }
+                if(ind!=-1){
+                    swap(arr[i],arr[ind]);
+                }else{
+                    break;
+                }
+
+            }
         }
         return arr;
     }
 };
-static auto init = []() { ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return nullptr; }();
