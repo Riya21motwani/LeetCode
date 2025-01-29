5beb443c9ccbb09c6e476aa2b8cb8e2430e43257
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-01-29 17:53:00 +0530

    Time: 37 ms (12.61%), Space: 116.8 MB (6.05%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,38 +1,39 @@
 class Solution {
-private:
-   
-    int countFishes(vector<vector<int>>& grid,
-                    int row, int col, int &sum, int &maxi) {
-        int n=grid.size();
-        int m=grid[0].size();
-        if(row<0 || col<0 || col>=m || row>=n|| grid[row][col]==0){
-            return 0;
-        }
-        sum+=grid[row][col];
-        grid[row][col]=0;
-        maxi=max(maxi,sum);
-        
-
-
-      
-        int drow[] = {0, 0, 1, -1};
-        int dcol[]= {1, -1, 0, 0};
-
-        for(int i=0;i<4;i++){
-            int nrow=drow[i]+row;
-            int ncol=col+dcol[i];
-            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]>0){
-                
-                
-                countFishes(grid,nrow,ncol,sum,maxi);
-            }
-        }
-      
-        return maxi;
-    }
-
 public:
    
+    int bfs(vector<vector<int>>& grid,int i, int j) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int sum=0;
+
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            sum+=grid[row][col];
+            grid[row][col]=0;
+
+            for(int k=0;k<4;k++){
+                int nrow=row+drow[k];
+                int ncol=col+dcol[k];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]>0){
+                    q.push({nrow,ncol});
+                }
+            }
+
+
+        }
+
+        return sum;
+
+    }
+
+
     int findMaxFish(vector<vector<int>>& grid) {
         int numRows = grid.size(), numCols = grid[0].size(), result = 0;
         vector<vector<bool>> visited(numRows, vector<bool>(numCols));
@@ -43,7 +44,7 @@ public:
                 int sum=0;
                 int maxi=0;
                 if (grid[i][j]>0) {
-                    result = max(result, countFishes(grid, i, j,sum,maxi));
+                    result = max(result, bfs(grid, i, j));
                 }
             }
         }
