22c780ce9730839db041eb688379080e4b015af2
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-02-13 16:33:03 +0530

    Time: 100 ms (38.73%), Space: 99.2 MB (16.98%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums, int k) {
-        for (int num : nums) {
-            if (num < k) return false;
-        }
-        return true;
-    }
 
     int minOperations(vector<int>& nums, int k) {
         int cnt = 0;
         
-        while (true) {
-            sort(nums.begin(), nums.end());
-            
-            if (nums.size() < 2) break;
-            
-            long long newVal = static_cast<long long>(nums[0]) * 2 + nums[1];  
-            nums.erase(nums.begin(), nums.begin() + 2);
-            nums.push_back(static_cast<int>(newVal)); 
+        
+
+        priority_queue<long long,vector<long long>,greater<long long>>pq;
+
+        for(auto it:nums){
+            pq.push(it);
+        }
+
+        while(!pq.empty()){
+            long long el1=pq.top();
+            pq.pop();
+            long long  el2=-1;
+            if (!pq.empty()){
+            el2=pq.top();
+           pq.pop();
+            }
+            if(el1>=k){
+                return cnt;
+            }
             cnt++;
 
-            if (isValid(nums, k)) return cnt;
+            long long  val=min(el1,el2)*2+max(el1,el2);
+
+            pq.push(val);
+
         }
 
         return cnt;
