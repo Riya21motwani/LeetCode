3fa65446d50c7093bdf9eae4b8890cb7999383d2
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-02-26 16:20:49 +0530

    Time: 1110 ms (23.79%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+
+-- | user_id | session_id | activity_date | activity_type |
+-- | ------- | ---------- | ------------- | ------------- |
+-- | 1       | 1          | 2019-07-20    | open_session  |
+-- | 1       | 1          | 2019-07-20    | scroll_down   |
+-- | 1       | 1          | 2019-07-20    | end_session   |
+-- | 2       | 4          | 2019-07-20    | open_session  |
+-- | 2       | 4          | 2019-07-21    | send_message  |
+-- | 2       | 4          | 2019-07-21    | end_session   |
+-- | 3       | 2          | 2019-07-21    | open_session  |
+-- | 3       | 2          | 2019-07-21    | send_message  |
+-- | 3       | 2          | 2019-07-21    | end_session   |
+
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+From Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+Group BY activity_date
+
+
