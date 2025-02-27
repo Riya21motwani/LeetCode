eaa3dfa8d9a830984bd91f4b9bd258521fe2aea9
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-02-27 13:10:54 +0530

    Time: 387 ms (20.94%), Space: 150 MB (13.25%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     unordered_map<int, int> umap; 
 
-    int findFibLength(int prev1, int prev2, vector<int>& arr) {
+    int findFibLength(int prev1, int prev2, vector<int>& arr,vector<vector<int>>&dp) {
         int n = arr.size();
         if (prev2 >= n) return 0;  
+        if(dp[prev1][prev2]!=-1){
+            return dp[prev1][prev2];
+        }
 
         int nextNum = arr[prev1] + arr[prev2];
         if (umap.find(nextNum) == umap.end()) return 0; 
 
         int nextIndex = umap[nextNum];
-        return 1 + findFibLength(prev2, nextIndex, arr);  
+        return dp[prev1][prev2]=1 + findFibLength(prev2, nextIndex, arr,dp);  
     }
 
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
         if (n < 3) return 0; 
+        vector<vector<int>>dp(n,vector<int>(n,-1));
         umap.clear(); 
         for (int i = 0; i < n; i++) {
             umap[arr[i]] = i; 
@@ -24,7 +28,7 @@ public:
         int maxLength = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int length = 2 + findFibLength(i, j, arr); 
+                int length = 2 + findFibLength(i, j, arr,dp); 
                 maxLength = max(maxLength, length);
             }
         }
