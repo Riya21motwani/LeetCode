d568660b5ab9160130692bd58265860cb4ebd220
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-02-27 13:37:10 +0530

    Time: 1053 ms (29.08%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -16,7 +16,7 @@
 
 SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 From Activity
-WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+WHERE activity_date  BETWEEN '2019-06-28' AND '2019-07-27'
 Group BY activity_date
 
 
