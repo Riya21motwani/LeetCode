5a85d050d49e70465889d8df3a8dc0156c1584bb
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-02-27 13:12:39 +0530

    Time: 442 ms (10.68%), Space: 150 MB (13.25%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    unordered_map<int, int> umap; 
-
-    int findFibLength(int prev1, int prev2, vector<int>& arr,vector<vector<int>>&dp) {
-        int n = arr.size();
-        if (prev2 >= n) return 0;  
-        if(dp[prev1][prev2]!=-1){
-            return dp[prev1][prev2];
+    
+    int f(int j, int k, unordered_map<int,int>&umap,vector<int>&arr, vector<vector<int>>&dp){
+        if(j<0){
+            return 0;
         }
-
-        int nextNum = arr[prev1] + arr[prev2];
-        if (umap.find(nextNum) == umap.end()) return 0; 
-
-        int nextIndex = umap[nextNum];
-        return dp[prev1][prev2]=1 + findFibLength(prev2, nextIndex, arr,dp);  
+        if(dp[j][k]!=-1){
+            return dp[j][k];
+        }
+        int val=arr[k]-arr[j];
+        if(umap.find(val)!=umap.end() && umap[val]<j){
+            return dp[j][k]= 1+ f(umap[val],j,umap,arr,dp);
+        }
+        return dp[j][k]=2;
     }
 
     int lenLongestFibSubseq(vector<int>& arr) {
         int n = arr.size();
         if (n < 3) return 0; 
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        umap.clear(); 
-        for (int i = 0; i < n; i++) {
-            umap[arr[i]] = i; 
+        int maxlen=0;
+        unordered_map<int,int>umap;
+        for(int i=0;i<n;i++){
+            umap[arr[i]]=i;
         }
-
-        int maxLength = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int length = 2 + findFibLength(i, j, arr,dp); 
-                maxLength = max(maxLength, length);
+        for(int j=1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                int len=f(j,k,umap,arr,dp);
+                if(len>=3){
+                    maxlen=max(maxlen,len);
+                }
             }
         }
-
-        return (maxLength >= 3) ? maxLength : 0; 
+       
+       return maxlen;
     }
 };
