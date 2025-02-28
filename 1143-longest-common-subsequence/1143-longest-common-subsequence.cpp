41bb8c96c9fc29d45b143fb960cf6fc5dbba44bb
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-02-28 11:45:43 +0530

    Time: 24 ms (70.41%), Space: 26 MB (78.41%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
 
-    int f(int i , int j ,int n,int m,string &text1 ,string &text2){
-
-
-        if(i==n && j==m){
-            return 0;
-
-        }
-        if(i==n || j==m){
+    int f(int n,int m,string &text1 ,string &text2,vector<vector<int>>&dp){
+        if(n==0 || m==0){
             return 0;
         }
 
+        if(dp[n][m]!=-1)return dp[n][m];
+        int profit=0;
+        if(text1[n-1]==text2[m-1]){
+            profit=1+f(n-1,m-1,text1 ,text2,dp);
+        }
+      else{
+        profit=max(f(n-1,m,text1 ,text2,dp),f(n,m-1,text1 ,text2,dp));
+      }
 
-       
-       if(text1[i]==text2[j]){
-        
-        return 1+f(i+1,j+1,n,m,text1,text2);
-        
-       }
-
-       return max(f(i,j+1,n,m,text1,text2) ,f(i+1,j,n,m,text1,text2) );
-       
+      return dp[n][m]=profit;
 
     }
 
@@ -29,8 +23,27 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-       
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-         return f(0,0,n,m,text1,text2);
+          for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int profit=0;
+               if(text1[i-1]==text2[j-1]){
+                profit=1+dp[i-1][j-1];
+               }
+               else{
+                      profit=max(dp[i-1][j],dp[i][j-1]);
+                }
+
+                  dp[i][j]=profit;
+            }
+          }
+
+
+
+
+         return dp[n][m];
+
+        // return f(n,m,text1,text2,dp);
     }
 };
