1a649f1a25c3b9223a164fbcfd935872b4f34f85
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-03-28 17:13:53 +0530

    Time: 71 ms (7.18%), Space: 81.6 MB (11.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    
-    
-//     int f(int i,int j, string word1 , string word2, vector<vector<int>>&dp){
-//         if(i==0)return j;
-//         if(j==0)return i;
-//         if(dp[i][j]!=-1)return dp[i][j];
-//         if(word1[i-1]==word2[j-1])return  dp[i][j]=f(i-1,j-1,word1,word2,dp);
-        
-//         return dp[i][j]=min(1+f(i,j-1,word1,word2,dp),min(1+f(i-1,j-1,word1,word2,dp),1+f(i-1,j,word1,word2,dp)));
-//     }
-    
-    
-    
-    
+
+    int f(int i, int j, string word1, string word2 , vector<vector<int>>&dp){
+        if(i==0 )return j;
+        if(j==0)return i;
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(word1[i-1]==word2[j-1]){
+            return dp[i][j]=f(i-1,j-1,word1,word2,dp);
+        }
+
+        return dp[i][j]=1+min(f(i,j-1,word1,word2,dp) , min(f(i-1,j,word1,word2,dp) , f(i-1,j-1,word1,word2,dp)));
+    }
+
     int minDistance(string word1, string word2) {
-       
         int n=word1.size();
         int m=word2.size();
-         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        // return f(n,m,word1,word2,dp);
-        
-        for(int i=0;i<=n;i++)dp[i][0]=i;
-        for(int j=0;j<=m;j++)dp[0][j]=j;
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
-        
-                else{
-                    dp[i][j]=1+min(dp[i][j-1],(min(dp[i-1][j-1],dp[i-1][j])));
-                }
-            }
-        }
-       
-            
-        
-        return dp[n][m];
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2,dp);
     }
-};   
+};
