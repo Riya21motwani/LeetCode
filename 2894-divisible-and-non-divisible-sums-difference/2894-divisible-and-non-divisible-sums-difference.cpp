65e53499ff5a4d4190cbfc85dd357c8f930599b6
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-05-27 20:23:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.04%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int cnt1=0;
+        int cnt2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m!=0){
+                cnt1+=i;
+            }else{
+                cnt2+=i;
+            }
+
+        }
+        return cnt1-cnt2;
+    }
+};
