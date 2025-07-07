e1f78d05172ac71b667d7a84b1d07b27d442172a
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-07-07 14:06:45 +0530

    Time: 87 ms (31.55%), Space: 92.1 MB (50.5%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -16,21 +16,25 @@ public:
         }
         return ans;
     }
-    int solve( int i,vector<vector<int>>&v){
+    int solve( int i,vector<vector<int>>&v , vector<int>&dp){
         int n=v.size();
         if(i>=n){
             return 0;
         }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
         int nexidx=f(i+1,v[i][1],v);
-        int take=v[i][2]+solve( nexidx ,v);
-        int notake=solve(i+1,v);
-        return max(take,notake);
+        int take=v[i][2]+solve( nexidx ,v,dp);
+        int notake=solve(i+1,v,dp);
+        return dp[i]=max(take,notake);
     }
 
 
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n=startTime.size();
         vector<vector<int>>v(n,vector<int>(3,0));
+        vector<int>dp(n, -1);
 
         for(int i=0;i<n;i++){
             v[i][0]=startTime[i];
@@ -39,6 +43,6 @@ public:
             
         }
         sort(v.begin(),v.end());
-        return solve(0,v);
+        return solve(0,v,dp);
     }
 };
