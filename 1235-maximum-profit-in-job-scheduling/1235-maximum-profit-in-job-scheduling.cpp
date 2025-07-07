6f3a2b6f67a1f3f01d8eae5d1791af79cc3459a1
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-07-07 14:06:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int f(int ind, int end , vector<vector<int>>&v){
+        int l=ind;
+        int n=v.size();
+        int h=n-1;
+        int ans=n+1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(end<=v[mid][0]){
+                ans=mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+    int solve( int i,vector<vector<int>>&v){
+        int n=v.size();
+        if(i>=n){
+            return 0;
+        }
+        int nexidx=f(i+1,v[i][1],v);
+        int take=v[i][2]+solve( nexidx ,v);
+        int notake=solve(i+1,v);
+        return max(take,notake);
+    }
+
+
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n=startTime.size();
+        vector<vector<int>>v(n,vector<int>(3,0));
+
+        for(int i=0;i<n;i++){
+            v[i][0]=startTime[i];
+            v[i][1]=endTime[i];
+           v[i][2]=profit[i];
+            
+        }
+        sort(v.begin(),v.end());
+        return solve(0,v);
+    }
+};
