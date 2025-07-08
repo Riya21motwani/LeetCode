de21d3d2bd88cb4994958c646b6e9ba7c47f4d52
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-07-08 11:17:15 +0530

    Time: 7 ms (5.76%), Space: 9.5 MB (11.54%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,37 +1,55 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-         stack<int> St;
-        stack<int> St1;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
-
-            if (ch == '(') {
-                St.push(i);
-            } else if (ch == '*') {
-                St1.push(i);
-            } else {
-                if (!St.empty()) {
-                    St.pop();
-                } else if (!St1.empty()) {
-                    St1.pop();
-                } else {
-                    return false;
+    bool f(int ind, int open, string s, int n , vector<vector<int>>&dp){
+        if(ind==n){
+            return open==0;
+        }
+        if(dp[ind][open]!=-1){
+            return dp[ind][open];
+        }
+        bool valid=false;
+        if(s[ind]=='*'){
+                valid = f(ind+1,open+1,s,n,dp) | f(ind+1,open, s,n,dp);
+                if(open>0){
+                    valid |=f(ind+1,open-1,s,n,dp);
                 }
-            }
+        }
+        else if(s[ind]=='('){
+            valid |=f(ind+1,open+1,s,n,dp);
+        }else if(open>0){
+            valid|=f(ind+1,open-1,s,n,dp);
         }
 
-       
-        while (!St.empty() && !St1.empty()) {
-            if (St.top() > St1.top()) {
-                return false;
-            }
-            St.pop();
-            St1.pop();
-        }
+        return dp[ind][open]=valid;
+    }
 
-        return St.empty();
-       
+    bool checkValidString(string s) {
+        
+        int n=s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // return f(0,0,s,n,dp);
+
+        dp[n][0]=true;
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int open=n-1;open>=0;open--){
+                bool valid=false;
+               if(s[ind]=='*'){
+                valid = dp[ind+1][open+1]| dp[ind+1][open] ;
+                 if(open>0){
+                      valid |=dp[ind+1][open-1];
+                  }
+                }
+              else if(s[ind]=='('){
+                 valid |=dp[ind+1][open+1];
+              }else if(open>0){
+            valid|=dp[ind+1][open-1];
+           }
+
+             dp[ind][open]=valid;
+            }
+        }
+        return dp[0][0];
     }
 };
