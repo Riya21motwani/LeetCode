637098f7c5470ff3128508b558bc4b0c81d0ff1e
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-08-16 09:00:36 +0530

    Time: 11 ms (76.27%), Space: 80.9 MB (50.77%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
@@ -4,25 +4,19 @@ public:
     typedef pair<int,int>p;
     int maximizeGreatness(vector<int>& nums) {
         int n=nums.size();
-        int cnt=0;
-        priority_queue<p,vector<p>,greater<p>>pq;
-        for(int i=0;i<nums.size();i++){
-            pq.push({nums[i],i});
-        }
         sort(nums.begin(),nums.end());
         int i=0;
-        while(!pq.empty()){
-            
-            while(!pq.empty() && nums[i]>=pq.top().first){
-                pq.pop();
-                
-            }
-            if(!pq.empty() && nums[i]<pq.top().first){
-                i++;
+        int j=0;
+        int cnt=0;
+        while(i<n && j<n){
+            if(nums[j]>nums[i]){
                 cnt++;
-                pq.pop();
+                i++;
+                j++;
+
+            }else{
+                j++;
             }
-            
         }
 
         return cnt;
