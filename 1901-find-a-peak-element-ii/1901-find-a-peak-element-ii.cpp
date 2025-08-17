98337af38f46f976ab240069e4b3d70f1a566a48
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-08-17 08:19:29 +0530

    Time: 0 ms (100%), Space: 50.5 MB (27.57%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    
-    
-    
-    int maximum(vector<vector<int>>& mat  , int  t, int n,int m){
-        int maxi=INT_MIN;
-        int ind=-1;
-        for(int i=0;i<n;i++){
-           if(mat[i][t] >maxi){
-
-            maxi=mat[i][t];
-               ind=i;
-           }
-        }
-           
-      return ind;
-    }
-    
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        int l=0;
-        int h=m-1;
-        while(l<=h){
-          
-            int mid=(l+h)/2;
-            int row=maximum(mat,mid,n,m);
-            
-            int left=mid-1>=0 ? mat[row][mid-1]:-1;
-            int right=mid+1<m ?mat[row][mid+1] :-1;
-            
-           
-            
-            if(left<mat[row][mid] &&  mat[row][mid]> right){
-                     return {row,mid};
-            }
-            else if( left>mat[row][mid]){
-                 h=mid-1;
-            }
-            else{
+        int n = mat.size(), m = mat[0].size();
+        int l = 0, r = m - 1;
 
-                  l=mid+1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+          
+            int maxRow = 0;
+            for (int i = 0; i < n; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
+                }
             }
-        
+
+            bool leftOk = (mid == 0) || (mat[maxRow][mid] > mat[maxRow][mid-1]);
+            bool rightOk = (mid == m-1) || (mat[maxRow][mid] > mat[maxRow][mid+1]);
+
+            if (leftOk && rightOk) {
+                return {maxRow, mid}; 
+            } else if (mid > 0 && mat[maxRow][mid-1] > mat[maxRow][mid]) {
+                r = mid - 1; 
+            } else {
+                l = mid + 1; 
+            }
         }
-        return {-1,-1};
+        return {-1, -1}; 
     }
 };
