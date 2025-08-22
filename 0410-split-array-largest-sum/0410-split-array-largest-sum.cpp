f84881b9571e7aa20cd200c903215687b90fd173
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-08-22 17:54:53 +0530

    Time: 2 ms (9.43%), Space: 10.6 MB (23.38%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
-    
-    int canfind( vector<int>& nums, int n , int idx){
+
+    bool possible(int &val,vector<int>& nums, int &k ){
         int sum=0;
         int cnt=1;
-        for(int i=0;i<n;i++){
-            if(nums[i]+sum<=idx){
+        for(int i=0;i<nums.size();i++){
+            if(sum+nums[i]<=val){
                 sum+=nums[i];
             }else{
-                cnt++;
+                cnt+=1;
                 sum=nums[i];
             }
         }
-        return cnt;
+        return cnt<=k;
     }
-    int sum(vector<int>& nums){
-        int summ=0;
-        for(int i=0;i<nums.size();i++){
-            summ+=nums[i];
-        }
-        return summ;
-    }
-    
-    
+
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
-        
-        int l=*max_element(nums.begin(),nums.end());
-        int h=sum(nums);
+        int total=0;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,nums[i]);
+            total+=nums[i];
+        }
+        int l=maxi;
+        int h=total;
         int ans=-1;
         while(l<=h){
             int mid=(l+h)/2;
-            
-             if(canfind(nums,n,mid) <=k){
-                ans=mid;
+            if(possible(mid,nums,k)){
                 h=mid-1;
-            }else if(canfind(nums,n,mid) >k){
-               l=mid+1;
+                ans=mid;
+                
+            }else{
+               
+                l=mid+1;
             }
         }
-        
+
         return ans;
+
     }
 };
