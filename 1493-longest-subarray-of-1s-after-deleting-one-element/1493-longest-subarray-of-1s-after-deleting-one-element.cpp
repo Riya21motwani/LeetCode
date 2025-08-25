ffd97afd1f9e4bd16bcd19376ae02861ce011e89
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-08-25 08:20:53 +0530

    Time: 0 ms (100%), Space: 60.2 MB (23.16%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,33 +2,25 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int n=nums.size();
+        int cnt0=0;
         int i=0;
         int j=0;
-        int cnt1=0;
-        int cnt0=0;
         int maxi=0;
         while(j<n){
-            
-            if(nums[j]==0)cnt0++;
-            if(nums[j]==1){
-                cnt1++;
-                
+            if(nums[j]==0){
+                cnt0+=1;
             }
-            if(cnt1==n){
-                return cnt1-1;
-            }
-            
-            
-                while(cnt0>1){
-                    if(nums[i]==0)cnt0--;
-                   else if(nums[i]==1)cnt1--;
-                    i++;
+
+            while(cnt0>1){
+                if(nums[i]==0){
+                    cnt0--;    
                 }
-            
+                i++;
+            }
+            maxi=max(maxi,j-i+1);
             j++;
-            maxi=max(maxi,cnt1);
-            
         }
-        return maxi;
+        
+        return maxi-1;
     }
 };
