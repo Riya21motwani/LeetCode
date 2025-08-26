2a2cf20c5678c2927b230bbeeaa9b24db01f0873
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-08-26 09:29:59 +0530

    Time: 1416 ms (24.01%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,49 @@
+-- Movies table:
+-- +-------------+--------------+
+-- | movie_id    |  title       |
+-- +-------------+--------------+
+-- | 1           | Avengers     |
+-- | 2           | Frozen 2     |
+-- | 3           | Joker        |
+-- +-------------+--------------+
+-- Users table:
+-- +-------------+--------------+
+-- | user_id     |  name        |
+-- +-------------+--------------+
+-- | 1           | Daniel       |
+-- | 2           | Monica       |
+-- | 3           | Maria        |
+-- | 4           | James        |
+-- +-------------+--------------+
+-- MovieRating table:
+-- +-------------+--------------+--------------+-------------+
+-- | movie_id    | user_id      | rating       | created_at  |
+-- +-------------+--------------+--------------+-------------+
+-- | 1           | 1            | 3            | 2020-01-12  |
+-- | 1           | 2            | 4            | 2020-02-11  |
+-- | 1           | 3            | 2            | 2020-02-12  |
+-- | 1           | 4            | 1            | 2020-01-01  |
+-- | 2           | 1            | 5            | 2020-02-17  | 
+-- | 2           | 2            | 2            | 2020-02-01  | 
+-- | 2           | 3            | 2            | 2020-03-01  |
+-- | 3           | 1            | 3            | 2020-02-22  | 
+-- | 3           | 2            | 4            | 2020-02-25  | 
+-- +-------------+--------------+--------------+-------------+
+
+
+
+(SELECT name AS results
+FROM users
+INNER JOIN MovieRating USING(user_id)
+GROUP BY user_id
+ORDER BY COUNT(rating) DESC , name
+LIMIT 1)
+UNION ALL
+(SELECT title AS results
+FROM Movies
+INNER JOIN MovieRating USING(movie_id)
+WHERE MONTH(created_at)='02'  AND YEAR(created_at)='2020'
+GROUP BY title
+ORDER BY AVG(rating) DESC ,title
+LIMIT 1)
+
