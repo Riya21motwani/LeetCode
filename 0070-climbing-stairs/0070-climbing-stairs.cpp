ee2c6ac828d1ae41a8d4409f9707db4c8e0574c8
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-08-31 16:24:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-   
-    int f(int n , vector<int>&dp){
-        if(n==0 || n==1){
+
+    int f(int n, vector<int>&dp){
+        if(n==1 || n==0){
             return 1;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        return dp[n]=f(n-1,dp) +f(n-2,dp);
+       int take=f(n-1,dp);
+       int notake=f(n-2,dp);
+        return dp[n]=take+notake;
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
+        vector<int>dp(46,-1);
         return f(n,dp);
     }
 };
