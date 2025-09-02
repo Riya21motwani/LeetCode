fed4e22ebd3cc40ed04f6acdde824544fea12341
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2025-09-02 20:40:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
 
-    int f(int ind, vector<int>& nums, vector<int>&dp){
+    int f(int i, vector<int>& nums,vector<int>&dp){
+
         int n=nums.size();
-        if(ind>=n){
+        if(i<0){
             return 0;
         }
-        if(dp[ind]!=-1){
-            return dp[ind];
+        if(dp[i]!=-1){
+            return dp[i];
         }
+        int take=nums[i]+f(i-2,nums,dp);
+        int notake=f(i-1,nums,dp);
+        return dp[i]=max(take,notake);
 
-        int take=nums[ind]+f(ind+2,nums,dp);
-        int notake=f(ind+1,nums,dp);
-
-        return dp[ind]=max(take,notake);
     }
 
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return f(0,nums,dp);
+        vector<int>dp(n,INT_MIN);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int take=0;
+            int notake=0;
+            if(i-2>=0){
+             take=nums[i]+dp[i-2];
+            }
+            if(i-1>=0){
+             notake=dp[i-1];
+            }
+             dp[i]=max(take,notake);
+        }
+        return dp[n-1];
     }
 };
