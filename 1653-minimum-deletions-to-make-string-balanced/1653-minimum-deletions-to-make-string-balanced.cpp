6d9f5a31fba43e2a2f13e4b3b2d6ebce4ce8357f
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-02-07 19:05:50 +0530

    Time: 71 ms (8.43%), Space: 55.2 MB (10.5%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-       stack<char>st;
-       int n=s.size();
-       int cnt=0;
-       
-       for(int i=0;i<n;i++){
-        if(!st.empty() && st.top()=='b' && s[i]=='a'){
-            cnt++;
-            st.pop();
-        }else{
-            st.push(s[i]);
-        }
-       }
-
+        int n=s.size();
+        vector<int>pre(n,0);
+        vector<int>post(n,0);
         
-       
-       return cnt;
+        for(int i=1;i<n;i++){
+            if(s[i-1]=='b'){
+                pre[i]=pre[i-1]+1;
+            }else{
+                pre[i]=pre[i-1];
+            }
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            if(s[i+1]=='a'){
+                post[i]=post[i+1]+1;
+            }else{
+                post[i]=post[i+1];
+            }
+        }
+        int mini=1e9;
+        for(int i=0;i<n;i++){
+            int maxi=(post[i]+pre[i]);
+            mini=min(mini,maxi);
+        }
+        return mini;
     }
-
 };
