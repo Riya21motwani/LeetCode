bae28aae7a7e43dc05abaf3faf408232cb31e4b9
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-04-01 21:23:53 +0530

    Time: 4 ms (89.58%), Space: 28.8 MB (94.41%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,60 +1,46 @@
 class Solution {
 public:
-    
-    
- 
-    
     int numEnclaves(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int cnt1=0;
         queue<pair<int,int>>q;
-        
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        
-         for(int i=0;i<n;i++){
-             for(int j=0;j<m;j++){
-                 
-                 if(i==0 || j==0 || i==n-1|| j==m-1){
-                     if(grid[i][j]==1){
-                         q.push({i,j});
-                         vis[i][j]=1;
-                     }
-                 }
-                 if(grid[i][j]==1){
-                     cnt1++;
-                 }
-             }
-         }
-        
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
+        int cnt1=0;
         int cnt=0;
+        int n=grid.size();
+        int drow[]={-1,1,0,0};
+        int dcol[]={0,0,-1,1};
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    cnt1++;
+                }
+                if((i==0 || i==n-1 || j==0 || j==m-1) && grid[i][j]==1){
+                    q.push({i,j});
+                    grid[i][j]=2;
+
+                }
+                
+            }
+        }
+
+
         while(!q.empty()){
-             
             int row=q.front().first;
             int col=q.front().second;
-            cnt++;
             q.pop();
-            
+            cnt++;
+
             for(int i=0;i<4;i++){
                 int nrow=row+drow[i];
                 int ncol=col+dcol[i];
-                if(nrow>=0 &&ncol>=0 &&nrow<n && ncol<m&& grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-                       q.push({nrow,ncol});
-                    vis[nrow][ncol]=1;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1){
+                    grid[nrow][ncol]=2;
+                    q.push({nrow,ncol});
                 }
             }
-            
-            
-            
-        
+
         }
-        
-        
+
         
         return cnt1-cnt;
-        
     }
 };
-
