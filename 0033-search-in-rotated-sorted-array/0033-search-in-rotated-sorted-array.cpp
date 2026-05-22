e9aa1997661a5acf3151246e08bee7438418ab2c
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-05-22 22:38:10 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,21 +5,21 @@ public:
         int l=0;
         int h=n-1;
         while(l<=h){
-            int mid=l+(h-l)/2;
-            if(target==nums[mid])return mid;
-            if(nums[mid]<=nums[h] ){
-                if(target>=nums[mid] && target<=nums[h]){
-                            l=mid+1;
+            int mid=(l+h)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && target <=nums[mid]){
+                    h=mid-1;
+                }else{
+                    l=mid+1;
+                }
+            }else{
+
+                if(nums[mid]<=target && target<=nums[h]){
+                    l=mid+1;
                 }else{
                     h=mid-1;
                 }
-            }
-            else{
-            if(nums[l]<=target && target <=nums[mid]){
-                    h=mid-1;
-            }else{
-                l=mid+1;
-            }
             }
         }
         return -1;
