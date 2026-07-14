11b1e3bd0e1505c6baaf8d2677893fb50c100ca7
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-07-14 08:10:20 +0530

    Time: 0 ms (100%), Space: 15 MB (19.35%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+       
+        unordered_map<int,int>umap;
+        for(int i=0;i<nums.size();i++){
+            if(umap.find(target-nums[i])==umap.end()){
+                umap[nums[i]]=i;
+            }
+            else{
+                return {(umap[target-nums[i]]),i};
+            }
+        }
+
+
+        return {-1,-1};
+    }
+};
