0b10bd9757ae22d6fd491fe01c8fc76c47dc83d1
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-07-14 09:10:23 +0530

    Time: 75 ms (22.09%), Space: 111.3 MB (13.89%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        int n=nums.size();
+        int maxi=0;
+        unordered_map<int,int>umap;
+        for(int i=0;i<n;i++){
+            umap[nums[i]]++;
+            if(umap[nums[i]]>1){
+                return true;
+            }
+        }
+        
+
+     return false;   
+    }
+};
