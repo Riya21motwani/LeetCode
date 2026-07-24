936c63f8d4c4af4ac2387516d4ead40d87814a09
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-07-24 09:37:36 +0530

    Time: 0 ms (100%), Space: 23 MB (56.39%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,33 +12,24 @@
 class Solution {
 public:
 
-    int f(TreeNode* root){
-
-         if(root==NULL ){
-            return 0;
-        }
-        if(root->left==NULL && root->right==NULL){
-            return 1;
-        }
-       
-
+    int f(TreeNode*root){
+        
+        if(root==NULL)return 0;
         int l=f(root->left);
         int r=f(root->right);
-        if(abs(r-l)>=2){
-            return -1;
-        }
-         
 
-         return 1+max(l,r);
+        if(l==-1)return -1;
+        if(r==-1)return -1;
+
+        if(abs(l-r)>=2)return -1;
+
+        return 1+max(l,r);
     }
-  
 
     bool isBalanced(TreeNode* root) {
-        if(f(root)==-1){
-            return false;
-        }
+        
+        if(f(root)==-1)return false;
 
         return true;
-       
     }
 };
