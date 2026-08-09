767292920d1d6cae73bcbc8d208c2cab5fda75aa
Riya Motwani <144720334+Riya21motwani@users.noreply.github.com>
2026-08-09 08:13:45 +0530

    Time: 4 ms (54.95%), Space: 18.5 MB (43.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
-    
-   bool dfs( int numCourses,  unordered_map<int,vector<int>>&adj, vector<int>&indegree , queue<int>&q  , vector<int>&ans){
-       for(int i=0;i<numCourses;i++){
-           if(indegree[i] ==0){
-                  q.push(i);
-           }
-       }
-      int cnt=0;
-      while(!q.empty()){
-           int node=q.front();
-           q.pop();
-          cnt++;
-           ans.push_back(node);
-           for(auto it:adj[node]){
-               indegree[it]--;
-              if(indegree[it]==0){
-                      q.push(it);
-              }
-           }
-       }
-       if(cnt==numCourses){
-           return true;
-       }
-        
-        return false;
-       
-   }
-    
-    
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>in(numCourses,0);
         unordered_map<int,vector<int>>adj;
-vector<int>indegree(numCourses,0);
-        for(auto it:prerequisites){
-            int a=it[0];
-            int b=it[1];
-            adj[b].push_back(a);
-            indegree[a]++;
-        }
-        vector<int>ans;
-        queue<int>q;
-      if((dfs( numCourses, adj, indegree , q , ans )==true)){
-          return ans;
-      }
         
-        return {};
+        for(int i=0;i<prerequisites.size();i++){
+            int a=prerequisites[i][0];
+            int b=prerequisites[i][1];
+            adj[b].push_back(a);
+            in[a]++;
+        }
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<in.size();i++){
+            if(in[i]==0){
+                q.push(i);
+                 ans.push_back(i);
+            }
+        }
+        int cnt=0;
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            cnt++;
+           
+            for(auto it:adj[node]){
+                in[it]--;
+                if(in[it]==0){
+                    q.push(it);
+                     ans.push_back(it);
+                }
+            }
+
+        }
+
+        if(cnt!=numCourses){
+            return {};
+        }
+
+
+        return ans;
+        
     }
 };
